9dda694edeb290b4646f76bfe9e11989cb8d479d
Jay Adkisson <jay@causes.com>
2010-07-01 13:45:09 -0700

    Object#cache_clear

--- lib/object.rb
+++ lib/object.rb
@@ -8,4 +8,11 @@ class Object
   def meta_eval(&blk)
     self.metaclass.class_eval(&blk)
   end
+
+  def cache_clear(ivar)
+    ivar = ivar.to_s
+    ivar.unlchomp!('@')
+    instance_variable_set(ivar, nil)
+  end
+
 end
